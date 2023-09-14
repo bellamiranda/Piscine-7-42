2ad27cb4b25ce895e37277cb32d2e8ad1ee77025
Isabella Miranda Camargo De Lima <ismirand@c1r2s5.42porto.com>
2023-09-14 18:34:26 +0100

    add

--- Treino/ft_split/ft_split.c
+++ Treino/ft_split/ft_split.c
@@ -6,9 +6,27 @@
 /*   By: ismirand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 18:15:24 by ismirand          #+#    #+#             */
-/*   Updated: 2023/09/13 19:39:09 by ismirand         ###   ########.fr       */
+/*   Updated: 2023/09/14 18:27:16 by ismirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strncpy(char *a, char *b, int k)
+{
+	int	i;
+
+	i = 0;
+	while (i < k)
+	{
+		a[i] = b[i];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
 
 char	**ft_split(char *str)
 {
@@ -18,6 +36,7 @@ char	**ft_split(char *str)
 	char	**dest;
 
 	i = 0;
+	j = 0;
 	while (str[i])
 	{
 		if ((str[i] > 32 && str[i] < 127) && 
@@ -25,25 +44,49 @@ char	**ft_split(char *str)
 			j++;
 		i++;
 	}
-	dest = (char **) malloc(sizeof(char *) * j + 1) //para o null
+	dest = (char **) malloc(sizeof(char *) * j + 1);
 	i = 0;
-	k = 0;
-	while (*str)
+	j = 0;
+	while (str[i])
 	{
-		if (str[0] > 32 && str[0] < 127)
+		k = 0;
+		while (str[i] > 32 && str[i] < 127)
 		{
 			i++;
-			str++;
+			k++;
 		}
-		dest[k] = (char *) malloc(sizeof(char) * i + 1);
-		
-		while (str[0] == 32 || str[0] == 9 || str[0] == 10)
-			str++;
-		i = 0;
-		k++;
+		dest[j] = (char *) malloc(sizeof(char) * k + 1);
+		dest[j] = ft_strncpy(dest[j], &str[i - k], k);
+		printf("%p\n", dest[j]);
+		j++;
+		while (!(str[i] > 32 && str[i] < 127))
+			i++;
 	}
+	dest[j] = 0;
+	return (dest);
 }
 
-/*while pra quantidade de palavras
-  conta tamanho de cada e faz malloc
-  copia cada uma pro dest[i][j] + NUL
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	char **dest;
+
+	i = 0;
+	if (argc == 2)
+		dest = ft_split(argv[1]);
+	while (dest[i] != 0)
+	{
+		j = 0;
+		while (dest[i][j] != '\0')
+		{
+			write(1, &dest[i][j], 1);
+			j++;
+		}
+		free (dest[i]);
+		write(1, ", ", 2);
+		i++;
+	}
+	write(1, "\n", 1);
+	free (dest);
+}
